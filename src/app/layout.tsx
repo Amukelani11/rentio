3f12bfb6eae2cd80ba622ce71320b799895d4a4c
kgahliso Maduna <kgahliso.maduna1@gmail.com>
2025-09-28 19:35:42 +0200

    Update favicon references to use proper favicon files from assets directory

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -96,9 +96,12 @@ export default function RootLayout({
         <link rel="dns-prefetch" href="https://www.google-analytics.com" />
 
         {/* Favicon */}
-        <link rel="icon" href="/assets/rentioicon.png" />
-        <link rel="apple-touch-icon" href="/assets/rentioicon.png" />
-        <link rel="shortcut icon" href="/assets/rentioicon.png" />
+        <link rel="icon" href="/assets/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png" />
+        <link rel="apple-touch-icon" href="/assets/apple-touch-icon.png" />
+        <link rel="manifest" href="/assets/site.webmanifest" />
+        <link rel="shortcut icon" href="/assets/favicon.ico" />
 
         {/* Theme color for mobile browsers */}
         <meta name="theme-color" content="#FF6B6B" media="(prefers-color-scheme: light)" />
